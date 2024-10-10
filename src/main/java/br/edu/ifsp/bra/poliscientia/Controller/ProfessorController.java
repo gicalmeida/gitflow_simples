b9cdeb4cd9a36d63ba1bca68d22e6784e22a6a6d
unknown <gicalmeidaa@gmail.com>
2024-10-10 17:03:05 -0300

    Post e gets do professor

--- src/main/java/br/edu/ifsp/bra/poliscientia/Controller/ProfessorController.java
+++ src/main/java/br/edu/ifsp/bra/poliscientia/Controller/ProfessorController.java
@@ -6,10 +6,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.ifsp.bra.poliscientia.Model.Aluno;
+import br.edu.ifsp.bra.poliscientia.Model.Professor;
 import br.edu.ifsp.bra.poliscientia.repository.ProfessorRepository;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 @RestController
 public class ProfessorController {
@@ -24,14 +29,38 @@ public class ProfessorController {
         listaAlunos.add(new Aluno("Franciele", 18, "franciele@gmail.com", "IFSP-BRA", 67890L));
     }
 
-    @GetMapping("/api/professor/listaDeAlunos") 
+    /*@GetMapping("/api/professor/listaDeAlunos") 
     public List<Aluno> endPoint1() { 
         return listaAlunos; 
-    } 
+    } */
 
     /*@GetMapping("/api/professor/buscarAluno{id_aluno}") 
     public Long endPoint2(@PathVariable("id_aluno") Long resultado){
         return resultado;
     }*/
+
+    @GetMapping("/api/professor/listaDeProfessores")
+    public List<Professor> getAllProfessor() {
+        return (List<Professor>)professorRepository.findAll();
+    }
+
+    @GetMapping("/api/professor/buscarAluno/{id_professor}")
+    public Professor getProfessorById(@PathVariable("id_professor")int id_professor){
+        return professorRepository.findById(id_professor).get();
+    }
+
+    @PostMapping("/professor")
+    public Professor createProfessor(@RequestBody Professor professor){
+    return professorRepository.save(professor);
+    }
+
+    /*@PutMapping("path/{id}")
+    public Professor putMethodName(@PathVariable("id_professor"), @RequestBody String entity) {
+        //TODO: process PUT request
+        
+        return entity;
+    }*/
+
+
 }
 
--- src/main/java/br/edu/ifsp/bra/poliscientia/Model/Professor.java
+++ src/main/java/br/edu/ifsp/bra/poliscientia/Model/Professor.java
@@ -16,33 +16,45 @@ public class Professor {
     private int idade;
     private String escola;
     
+    Professor(){
+    }
+
     public String getNome_professor() {
         return nome_professor;
     }
+
     public void setNome_professor(String nome_professor) {
         this.nome_professor = nome_professor;
     }
+
     public String getEmail_professor() {
         return email_professor;
     }
+
     public void setEmail_professor(String email_professor) {
         this.email_professor = email_professor;
     }
+
     public int getIdade() {
         return idade;
     }
+
     public void setIdade(int idade) {
         this.idade = idade;
     }
+
     public String getEscola() {
         return escola;
     }
+
     public void setEscola(String escola) {
         this.escola = escola;
     }
+
     public int getId_professor() {
         return id_professor;
     }
+    
     public void setId_professor(int id_professor) {
         this.id_professor = id_professor;
     }
