3585977f0de9335b90692547173fdbf0388ce8de
unknown <gicalmeidaa@gmail.com>
2024-10-14 16:02:34 -0300

    Put no controller do professor e repository do aluno

--- src/main/java/br/edu/ifsp/bra/poliscientia/Controller/ProfessorController.java
+++ src/main/java/br/edu/ifsp/bra/poliscientia/Controller/ProfessorController.java
@@ -2,18 +2,25 @@ package br.edu.ifsp.bra.poliscientia.Controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import br.edu.ifsp.bra.poliscientia.Model.Aluno;
 import br.edu.ifsp.bra.poliscientia.Model.Professor;
 import br.edu.ifsp.bra.poliscientia.repository.ProfessorRepository;
-import org.springframework.web.bind.annotation.PutMapping;
+import br.edu.ifsp.bra.poliscientia.repository.AlunoRepository;
+
 
 
 @RestController
@@ -21,45 +28,56 @@ public class ProfessorController {
     @Autowired
     ProfessorRepository professorRepository;
 
+    AlunoRepository alunoRepository;
+
     ArrayList<Aluno> listaAlunos = new ArrayList<>();
 
     
-    public ProfessorController() {
+   /*  public ProfessorController() {
         listaAlunos.add(new Aluno("Giovana", 17, "giovana@gmail.com", "IFSP-BRA", 12345L));
         listaAlunos.add(new Aluno("Franciele", 18, "franciele@gmail.com", "IFSP-BRA", 67890L));
-    }
-
-    /*@GetMapping("/api/professor/listaDeAlunos") 
-    public List<Aluno> endPoint1() { 
-        return listaAlunos; 
-    } */
-
-    /*@GetMapping("/api/professor/buscarAluno{id_aluno}") 
-    public Long endPoint2(@PathVariable("id_aluno") Long resultado){
-        return resultado;
     }*/
 
-    @GetMapping("/api/professor/listaDeProfessores")
-    public List<Professor> getAllProfessor() {
-        return (List<Professor>)professorRepository.findAll();
+    @GetMapping("/api/professor/listaDeAlunos")
+    public List<Aluno> getAllAluno() {
+        return (List<Aluno>)alunoRepository.findAll();
     }
 
-    @GetMapping("/api/professor/buscarAluno/{id_professor}")
-    public Professor getProfessorById(@PathVariable("id_professor")int id_professor){
-        return professorRepository.findById(id_professor).get();
+    @GetMapping("/api/professor/buscarAluno/{id_aluno}")
+    public Aluno getProfessorById(@PathVariable("id_aluno")int id_aluno){
+        return alunoRepository.findById((long) id_aluno).get();
     }
 
-    @PostMapping("/professor")
+    @PostMapping("/api/professor")
     public Professor createProfessor(@RequestBody Professor professor){
     return professorRepository.save(professor);
     }
 
-    /*@PutMapping("path/{id}")
-    public Professor putMethodName(@PathVariable("id_professor"), @RequestBody String entity) {
-        //TODO: process PUT request
+    @PutMapping("/api/adicionarAluno/{id_aluno}")
+    public ResponseEntity<Aluno> putMethodName(@PathVariable("id_aluno") Long id_aluno, @RequestBody Aluno aluno) {
+        Optional<Aluno> alunoExistente = alunoRepository.findById(id_aluno);
         
-        return entity;
-    }*/
+        if (alunoExistente.isPresent()) {
+            // Atualiza o aluno existente
+            Aluno alunoAtualizado = alunoExistente.get();
+            alunoAtualizado.setNome(aluno.getNome());
+            alunoAtualizado.setEmail_aluno(aluno.getEmail_aluno());
+            alunoAtualizado.setIdade(aluno.getIdade());
+            alunoAtualizado.setEscola(aluno.getEscola());
+            alunoAtualizado.setSenha(aluno.getSenha());
+
+            Aluno alunoSalvo = alunoRepository.save(alunoAtualizado); // Salva as alterações no banco de dados
+            return ResponseEntity.ok(alunoSalvo); // Retorna 200 OK com o aluno atualizado
+        } else {
+            // Cria um novo aluno, se não encontrado
+            aluno.setId_aluno(id_aluno); // Define o ID para o novo aluno
+            Aluno novoAluno = alunoRepository.save(aluno); // Salva o novo aluno no banco de dados
+            return ResponseEntity.status(HttpStatus.CREATED).body(novoAluno); // Retorna 201 Created com o novo aluno
+        }
+    }
+
+    /*@DeleteMapping ("/api/removerAluno/{email_aluno}")
+    public Aluno ()*/
 
 
 }
---
+++ src/main/java/br/edu/ifsp/bra/poliscientia/repository/AlunoRepository.java
@@ -0,0 +1,9 @@
+package br.edu.ifsp.bra.poliscientia.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.edu.ifsp.bra.poliscientia.Model.Aluno;
+
+public interface AlunoRepository extends CrudRepository<Aluno, Long>{
+    
+}
